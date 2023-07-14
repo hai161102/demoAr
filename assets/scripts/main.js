746af3fb9f5ac87e789f8d31ac367a5e14e31999
hai161102 <ripallrank@gmail.com>
2023-07-14 17:52:50 +0700

    update

---
+++ assets/scripts/main.js
@@ -0,0 +1,24 @@
+
+// import * as AFRAME from 'aframe';
+
+// AFRAME.registerComponent('girlModel', {
+//     schema: {
+        
+//     },
+
+//     init: function () {
+//       // Do something when component first attached.
+//     },
+
+//     update: function () {
+//       // Do something when component's data is updated.
+//     },
+
+//     remove: function () {
+//       // Do something the component or its entity is detached.
+//     },
+
+//     tick: function (time, timeDelta) {
+//       // Do something on every scene tick or frame.
+//     }
+// });
